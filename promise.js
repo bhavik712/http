4268f29488ffe01d311cbe4980d84e1e48f69dfc
bhavik712 <baradbhavik41@gmail.com>
2023-07-20 16:56:06 +0530

    use of promise chaining

---
+++ promise.js
@@ -0,0 +1,25 @@
+
+// const divide = (a,b) => new Promise((resolve, reject)=>{
+//     b ? resolve(a/b) : reject('divider should not be zero')
+// })
+
+// divide(4,2).then((data)=>{
+//     return divide(4, data-1)
+//     }).then((data)=>{console.log(data)
+//     }).catch((err)=>console.log(err)) ;
+
+ const mul = (num) => new Promise((resolve,reject)=>{
+    typeof num === 'number' ? resolve(num*2) : reject ('it is not number');
+ })
+
+//  mul(2).then((data)=>console.log(data), (err)=>console.log(err));
+
+mul('3').then((data)=>{
+    return mul(data)
+}).then((data)=>{
+    return mul(data)
+}).then((data)=>{
+    return mul(data)
+}).then((data)=>{
+    console.log(data)
+}).catch((err)=>console.log(err));
