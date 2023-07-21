8b0f5f7e68b3b37ecc8570cd6990c1740b0160d9
bhavik712 <baradbhavik41@gmail.com>
2023-07-21 11:14:41 +0530

    use of async await

--- my.js
+++ my.js
@@ -1,15 +1,27 @@
-const myDetails = () => fetch('https://ipinfo.io/?token=9182aa3f9708b7',{}).then((response)=>{
+const myDetails = async () => {
+    const response = await fetch('https://ipinfo.io/?token=9182aa3f9708b7')
     if(response.status == 200){
-        return response.json();
+        const data = await response.json();
+        return data;
     }
     else{
         throw new Error('no data available');
     }
-}).catch((error)=>console.log(error))
+}
 
-myDetails().then((data)=>{
-    document.querySelector('#city').textContent = data.city;
-    document.querySelector('#region').textContent = data.region;
-    document.querySelector('#country').textContent = data.country;
+// myDetails().then((data)=>document.querySelector('#city').textContent = data.city);
 
-})
+const showDetails = async ()=>{
+    const data = await myDetails();
+    const cityName = data.city;
+    document.querySelector('#city').textContent = cityName;
+}
+
+showDetails();
+
+// myDetails().then((data)=>{
+//     
+//     document.querySelector('#region').textContent = data.region;
+//     document.querySelector('#country').textContent = data.country;
+
+// }).catch((error)=>console.log(error))
