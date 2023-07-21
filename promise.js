2d3ef84f8c8fd2b04faac756ebafd19bed459eee
bhavik712 <baradbhavik41@gmail.com>
2023-07-21 10:40:54 +0530

    use of aysnc await

--- promise.js
+++ promise.js
@@ -8,18 +8,25 @@
 //     }).then((data)=>{console.log(data)
 //     }).catch((err)=>console.log(err)) ;
 
- const mul = (num) => new Promise((resolve,reject)=>{
+ const mul =  (num) => new Promise((resolve,reject)=>{
     typeof num === 'number' ? resolve(num*2) : reject ('it is not number');
- })
+   })
 
+const getdata = async () =>{
+    let result = await mul(2);
+    result = await mul(result);
+    return result;
+}
+
+console.log(getdata());
 //  mul(2).then((data)=>console.log(data), (err)=>console.log(err));
 
-mul('3').then((data)=>{
-    return mul(data)
-}).then((data)=>{
-    return mul(data)
-}).then((data)=>{
-    return mul(data)
-}).then((data)=>{
-    console.log(data)
-}).catch((err)=>console.log(err));
+// mul('3').then((data)=>{
+//     return mul(data)
+// }).then((data)=>{
+//     return mul(data)
+// }).then((data)=>{
+//     return mul(data)
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((err)=>console.log(err));
